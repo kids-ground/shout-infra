05a70b511894f9b30f7e662a78ded2a6e4a1d1dd
RokwonK <wonrok97@naver.com>
2023-06-27 22:31:14 +0900

    ✨ main variables 추가

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" { }
+
+variable "domain_name" { }
+variable "ns_records" { }
+variable "api_server_domain" { }
+
+variable "vpc_cidr" { }
+variable "az_names" { }
+variable "public_subnets" { }
+variable "private_subnets" { }
+
+variable "ec2_inbound_rule" { }
+variable "db_inbound_rule" { }
+variable "outbound_rule" { }
